4b727f0add92efa87e4cb6cc52dfb6f4df780415
Retsel <retsel@640x480.info>
2025-05-02 16:14:43 -0400

    List functionality works enough for now, from the look of it (no actual archived projects to test)

--- pat.py
+++ pat.py
@@ -1,5 +1,7 @@
+import argparse
 import os
 import sqlite3
+import sys
 from urllib.request import pathname2url
 
 class CaidoUtil:
@@ -45,13 +47,16 @@ class CaidoUtil:
             results.append({'id': row[0], 'name': row[1]})
         return results
 
-
-
-
-
-
-#get "archived" projects
-
+    def get_archived_projects(self):
+        # Get "archived" projects - tgz files like NAME-UUID.tgz in the data path
+        dlist = os.listdir(self.data_path)
+        archived_projects = []
+        for fname in dlist:
+            if fname.endswith('.tgz'):
+                fnparts = fname[:-4].rsplit('-',5)
+                if len(fnparts) == 6:
+                    archived_projects.append({'id': '-'.join(fnparts[1:]), 'name': fnparts[0]})
+        return archived_projects
 
 
 
@@ -80,3 +85,32 @@ class CaidoUtil:
 
 
 #include an option for --data-path
+if __name__ == '__main__':
+    cutil = CaidoUtil()
+    parser = argparse.ArgumentParser(
+            prog='pat-caido',
+            description='Project Archive Tool for Caido Workspaces',
+            epilog='')
+    parser.add_argument('operation', choices=['list', 'archive', 'restore'])
+    parser.add_argument('wsname', default=None, nargs='?')
+    args = parser.parse_args()
+    if args.operation == 'list':
+        active = cutil.get_active_projects()
+        archived = cutil.get_archived_projects()
+        print('Active Workspaces:')
+        for ap in active:
+            print('{name} ({projectid})'.format(name=ap['name'], projectid=ap['id']))
+        print('')
+        print('-------------------')
+        print('Archived Workspaces:')
+        for ap in archived:
+            print('{name} ({projectid})'.format(name=ap['name'], projectid=ap['id']))
+        print('')
+    else:
+        if args.wsname == None:
+            print('error: operation requires a workspace name')
+        if args.operation == 'archive':
+            pass
+        elif args.operation == 'restore':
+            pass
+        
