56b5ddd3345089d6b551f493cafdcecbaae80e63
Retsel <retsel@640x480.info>
2025-05-24 18:47:58 -0400

    Pylint fixes, score change from 8.24 to 9.73. Didn't start all that bad, considering.

--- pat.py
+++ pat.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python
+"""
+Project Archive Tool for Caido Workspaces
+
+List, Archive, and Restore Caido workspaces/projects
+Note that, by design, this works best when it modifies the Caido sqlite3 database
+...so, at your own risk, and the schema may change.
+"""
 import argparse
 import os
 import shutil
@@ -8,12 +15,13 @@ import tarfile
 from urllib.request import pathname2url
 
 class CaidoUtil:
+    """ Helper utility for isolating the interaction with Caido's home"""
 
     def __init__(self, caido_home=None, read_only=True):
         self.data_path = caido_home
         self.read_only = read_only
         # Lets us reuse the open helper function to reopen r/w
-        self.db = None
+        self.db_conn = None
         if not caido_home:
             self.data_path = self.get_data_path()
         self.project_path = os.path.join(self.data_path, 'projects')
@@ -22,26 +30,27 @@ class CaidoUtil:
 
     def __del__(self):
         ''' Clean self up'''
-        if self.db != None:
-            self.db.close()
+        if self.db_conn is not None:
+            self.db_conn.close()
         # Everything else should just be boring things we don't care about
 
     def __open_db(self):
         ''' Open or reopen the DB at db_file, based on current state of read_only'''
-        if self.db != None:
-            self.db.close()
-            self.db = None
+        if self.db_conn is not None:
+            self.db_conn.close()
+            self.db_conn = None
+        path_as_url = pathname2url(self.db_file)
         if self.read_only:
-            db_uri = 'file:{}?mode=r'.format(pathname2url(self.db_file))
+            db_uri = f'file:{path_as_url}?mode=ro'
         else:
-            db_uri = 'file:{}?mode=rw'.format(pathname2url(self.db_file))
+            db_uri = f'file:{path_as_url}?mode=rw'
         # Use uri-like path to prevent file creation
         try:
-            self.db = sqlite3.connect(self.db_file, uri=True)
+            self.db_conn = sqlite3.connect(db_uri, uri=True)
         except sqlite3.OperationalError:
             # missing DB
             self.data_path = None
-            self.db = None
+            self.db_conn = None
             raise
 
     def get_data_path(self):
@@ -59,10 +68,11 @@ class CaidoUtil:
         testpath = os.path.join(userhome, 'caido', 'Caido', 'data')
         if os.path.exists(testpath):
             return testpath
+        return None
 
     def get_active_projects(self):
         ''' Get active projects from sqlite'''
-        cur = self.db.cursor()
+        cur = self.db_conn.cursor()
         res = cur.execute("SELECT id, name FROM projects")
         loft =  res.fetchall()
         results = []
@@ -92,11 +102,11 @@ class CaidoUtil:
     def get_project_directory_by_name(self, name):
         ''' Return the full path for a provided project name. Requires existence.'''
         candidates = self.get_active_projects()
-        for ap in candidates:
-            if ap['name'] == name:
-                return os.path.join(self.project_path, ap['id'])
+        for candp in candidates:
+            if candp['name'] == name:
+                return os.path.join(self.project_path, candp['id'])
         return None
-    
+
     def get_archive_file_by_id(self, projectid):
         ''' Look up the path for an archived project by ID'''
         dlist = os.listdir(self.project_path)
@@ -105,7 +115,7 @@ class CaidoUtil:
                 fnparts = fname[:-4].rsplit('-',5)
                 if len(fnparts) == 6:
                     found_id = fnparts[1:]
-                    found_name = fnparts[0]
+                    #found_name = fnparts[0]
                     if projectid == found_id:
                         return fname
         return None
@@ -117,12 +127,12 @@ class CaidoUtil:
             if fname.endswith('.tgz'):
                 fnparts = fname[:-4].rsplit('-',5)
                 if len(fnparts) == 6:
-                    found_id = fnparts[1:]
+                    #found_id = fnparts[1:]
                     found_name = fnparts[0]
                     if name == found_name:
                         return fname
         return None
-    
+
     def get_archive_directory(self):
         ''' Default archive directory is same as project path, but reasonable to be changed'''
         return self.project_path
@@ -138,36 +148,37 @@ class CaidoUtil:
     def db_add(self, projectid):
         ''' Read the project config to re-insert the project record'''
         src_file = os.path.join(self.get_project_directory_by_id(projectid), 'metadata.txt')
-        cur = self.db.cursor()
-        with open(src_file, 'r') as fin:
-            ls = fin.read()
-            parts = ls.strip().split('\n')
+        cur = self.db_conn.cursor()
+        with open(src_file, 'rt', encoding='utf8') as fin:
+            lines = fin.read()
+            parts = lines.strip().split('\n')
             # print(parts)
             try:
-                res = cur.execute('INSERT INTO projects \
+                _ = cur.execute('INSERT INTO projects \
                         (id, name, version, created_at, updated_at, status, selected_at) \
                         VALUES (?,?,?,?,?,?,?)', parts)
-                self.db.commit()
+                self.db_conn.commit()
             except sqlite3.IntegrityError:
                 print('Project name already found in DB. Not able to INSERT record.')
 
     def db_remove(self, projectid):
         ''' Remove the project from the db, based on project ID'''
-        cur = self.db.cursor()
+        cur = self.db_conn.cursor()
         # I don't like testing this part.
-        res = cur.execute('DELETE FROM projects WHERE id = ?', (projectid,))
-        self.db.commit()
+        _ = cur.execute('DELETE FROM projects WHERE id = ?', (projectid,))
+        self.db_conn.commit()
 
     def db_record(self, projectid):
         ''' Create the project config txt file in the project directory from the DB record'''
-        cur = self.db.cursor()
-        res = cur.execute('SELECT id, name, version, created_at, updated_at, status, selected_at FROM projects WHERE id = ?', (projectid,))
+        cur = self.db_conn.cursor()
+        _ = cur.execute('SELECT id, name, version, created_at, updated_at, status, selected_at \
+                FROM projects WHERE id = ?', (projectid,))
         result_record = cur.fetchone()
         # Maybe add a 'verbose' flag at some point.
         #print(result_record)
         dest_dir = self.get_project_directory_by_id(projectid)
         dest_file = os.path.join(dest_dir, 'metadata.txt')
-        with open(dest_file, 'w') as fout:
+        with open(dest_file, 'wt', encoding='utf8') as fout:
             for rec in result_record:
                 fout.write(rec)
                 fout.write('\n')
@@ -187,38 +198,38 @@ if __name__ == '__main__':
     parser.add_argument('-m', '--modify', action='store_true',
             help='Modify the Caido projects.db file to reflect changes')
     args = parser.parse_args()
-    cutil = CaidoUtil(read_only = args.modify)
+    cutil = CaidoUtil(read_only = not args.modify)
     if args.operation == 'list':
         active = cutil.get_active_projects()
         archived = cutil.get_archived_projects()
         print('Active Workspaces:')
-        for ap in active:
-            print('{name} ({projectid})'.format(name=ap['name'], projectid=ap['id']))
+        for acp in active:
+            print(f'{acp["name"]} ({acp["id"]})')
         print('')
         print('-------------------')
         print('Archived Workspaces:')
-        for ap in archived:
-            print('{name} ({projectid})'.format(name=ap['name'], projectid=ap['id']))
+        for arp in archived:
+            print(f'{arp["name"]} ({arp["id"]})')
         print('')
     else:
-        if args.wsname == None:
+        if args.wsname is None:
             print('error: operation requires a workspace name')
             sys.exit(1)
         if args.operation == 'archive':
-            # It's about here where I wonder if I should have {name: id} instead 
+            # It's about here where I wonder if I should have {name: id} instead
             # Shouldn't be a problem even to linear search the restore list
             #   even if it has a handful of archived projects.
             active = cutil.get_active_projects()
             archive_target = None
-            for ap in active:
-                if ap['name'] == args.wsname:
-                    archive_target = ap
+            for acp in active:
+                if acp['name'] == args.wsname:
+                    archive_target = acp
                     break
             else:
                 print('error: unable to find workspace')
                 sys.exit(2)
             # archive the folder in the uuid directory
-            tgz_name = '%s-%s.tgz' % (archive_target['name'], archive_target['id'])
+            tgz_name = f'{archive_target["name"]}-{archive_target["id"]}.tgz'
             tgz_path = os.path.join(cutil.get_archive_directory(), tgz_name)
             src_directory = cutil.get_project_directory_by_id(archive_target['id'])
             cutil.db_record(archive_target['id'])
@@ -235,15 +246,15 @@ if __name__ == '__main__':
         elif args.operation == 'restore':
             archived = cutil.get_archived_projects()
             archive_target = None
-            for ap in archived:
-                if ap['name'] == args.wsname:
-                    archive_target = ap
+            for arp in archived:
+                if arp['name'] == args.wsname:
+                    archive_target = arp
                     break
             else:
                 print('error: unable to find workspace archive')
                 sys.exit(3)
             # uncompress the archive to the uuid folder name
-            tgz_name = '%s-%s.tgz' % (archive_target['name'], archive_target['id'])
+            tgz_name = f'{archive_target["name"]}-{archive_target["id"]}.tgz'
             tgz_path = os.path.join(cutil.get_archive_directory(), tgz_name)
             # Shouldn't need more than the project base directory
             dst_directory = cutil.get_project_directory_by_id(archive_target['id'])
@@ -259,5 +270,3 @@ if __name__ == '__main__':
             else:
                 print('Complete. Removing workspace archive.')
                 os.remove(tgz_path)
-            pass
-        
